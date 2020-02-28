0858211d9ffd9851003e822268631bce14b967e1
bcagliari <55242720+bcagliari@users.noreply.github.com>
2020-02-28 17:43:30 -0300

    Testbench

---
+++ HLS/hlss_tb.vhd
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   02:46:36 12/19/2019
+-- Design Name:   
+-- Module Name:   D:/Documents/workspace/vhdl/hlss/project/hlss_tb.vhd
+-- Project Name:  hlss
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: main
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY hlss_tb IS
+END hlss_tb;
+ 
+ARCHITECTURE behavior OF hlss_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT main
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         start : IN  std_logic;
+         done : OUT  std_logic;
+         media : OUT  std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+   signal start : std_logic := '0';
+
+ 	--Outputs
+   signal done : std_logic;
+   signal media : std_logic_vector(15 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: main PORT MAP (
+          clk => clk,
+          rst => rst,
+          start => start,
+          done => done,
+          media => media
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      rst <= '1';
+      wait for 100 ns;	
+		rst <= '0';
+		start <= '1';
+      wait for 100 ns;	
+		start <= '0';
+
+      wait;
+   end process;
+
+END;
